6abe2f29352e538d42bf7e2797971291378a6922
Jacob Lang <jacoblang72@comcast.net>
2023-10-09 22:39:47 -0400

    redid problems to practice

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -9,6 +9,138 @@ module.exports = {
 }
 
 async function findPosts(user_id) {
+  
+  const userPosts = await db("posts as p")
+  .leftJoin("users as u","p.user_id", "u.id")
+  .select("p.id as post_id","contents","username")
+  .where("user_id", user_id);
+  return userPosts; 
+
+  /*
+
+    Implement so it resolves this structure:
+select 
+p.id as post_id,
+contents,
+username
+from posts as p
+left join users as u
+on p.user_id = u.id;
+    [
+      {
+          "post_id": 10,
+          "contents": "Trusting everyone is...",
+          "username": "seneca"
+      },
+      etc
+    ]
+  */
+
+}
+
+async function find() {
+  const found = await db("users as u")
+  .leftJoin("posts as p","u.id","p.user_id")
+  .select("u.id as user_id","username")
+  .count("p.id as postcount")
+  .groupBy("user_id");
+  return found; 
+  /*
+  select 
+u.id as user_id,
+username,
+count(p.user_id) as postcount
+from users as u 
+left join posts as p
+on u.id = p.user_id
+group by p.user_id
+order by postcount desc;
+    [
+        {
+            "user_id": 1,
+            "username": "lao_tzu",
+            "post_count": 6
+        },
+        {
+            "user_id": 2,
+            "username": "socrates",
+            "post_count": 3
+        },
+        etc
+    ]
+  */
+}
+
+async function findById(id) {
+const foundById = await db("users as u")
+.leftJoin("posts as p","u.id","p.user_id")
+.select("u.id as user_id","username","contents","p.id as post_id")
+.where({user_id : id});
+
+const posts = [];
+for (let key of foundById) {
+  posts.push({
+    post_id : key.post_id,
+    contents : key.contents
+  })
+}
+const returnObj = {
+  user_id : foundById[0].user_id,
+  username : foundById[0].username,
+  posts : posts
+}
+return returnObj; 
+  /*
+    Improve so it resolves this structure:
+select 
+u.id as user_id,
+username,
+contents
+from users as u left join 
+posts as p 
+on u.id = p.user_id
+where u.id = 1;
+    {
+      "user_id": 2,
+      "username": "socrates"
+      "posts": [
+        {
+          "post_id": 7,
+          "contents": "Beware of the barrenness of a busy life."
+        },
+        etc
+      ]
+    }
+  */
+}
+
+function add(user) {
+  return db('users')
+    .insert(user)
+    .then(([id]) => { // eslint-disable-line
+      return findById(id)
+    })
+}
+
+function remove(id) {
+  // returns removed count
+  return db('users').where({ id }).del()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+async function findPosts(user_id) {
 
   const rows = await db("posts as p")
     .select("p.id as post_id", "contents", "username")
@@ -34,7 +166,7 @@ on p.user_id = u.id;
       etc
     ]
   */
-
+/*
 }
 
 async function find() {
@@ -69,6 +201,7 @@ group by u.id;
         etc
     ]
   */
+ /*
 }
 
 async function findById(id) {
@@ -83,14 +216,12 @@ async function findById(id) {
     .where({ user_id: id });
   const posts = [];
   for (let key of unStructuredData) {
-    if (key.contents) {
       posts.push(
         {
           post_id: key.post_id,
           contents: key.contents,
         }
       )
-    }
   }
   const returnObj = {
     user_id: unStructuredData[0].user_id,
@@ -121,17 +252,8 @@ where u.id = 1;
       ]
     }
   */
+ /*
 }
+*/
 
-function add(user) {
-  return db('users')
-    .insert(user)
-    .then(([id]) => { // eslint-disable-line
-      return findById(id)
-    })
-}
 
-function remove(id) {
-  // returns removed count
-  return db('users').where({ id }).del()
-}
